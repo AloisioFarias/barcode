cca8086b30449e582d467af43bac5e49e3955324
aloisiofarias <aloisio.farias@gmail.com>
2016-11-01 21:50:13 -0200

    20161101

---
+++ Barcode/WebContent/scripts.js
@@ -0,0 +1,28 @@
+var imgArray = new Array();
+
+imgArray[0] = new Image();
+imgArray[0].src = "./css2/images/FLUXO_LOGIN.jpg";
+
+imgArray[1] = new Image();
+imgArray[1].src = "./css2/images/FLUXO_COMPRA.jpg";
+
+imgArray[2] = new Image();
+imgArray[2].src = "./css2/images/FLUXO_INGRED.jpg";
+
+imgArray[3] = new Image();
+imgArray[3].src = "./css2/images/FLUXO_INGRED.jpg";
+
+imgArray[4] = new Image();
+imgArray[4].src = "./css2/images/FLUXO_INGRED.jpg";
+
+imgArray[5] = new Image();
+imgArray[5].src = "./css2/images/FLUXO_INGRED.jpg";
+
+window.onload = mostraImagem(index);
+
+function mostraImagem(index) {
+	
+	var img = document.createElement("IMG");
+	document.getElementById('id').appendChild(imgArray[index]);
+	
+}
